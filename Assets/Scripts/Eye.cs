e6b18204a9300e94f36570cd5d97967473abb669
Tangel2002 <90701100+Tangel2002@users.noreply.github.com>
2022-04-09 14:32:24 -0400

    added movement and eye tracking

---
+++ Assets/Scripts/Eye.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform eye;
+
+    void Update()
+    {
+        Vector2 Direction = target.position - transform.position;
+        float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
+        Quaternion lookRot = Quaternion.AngleAxis(angle, Vector3.forward);
+        eye.rotation = Quaternion.Slerp(transform.rotation, lookRot, 20f * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Transform target;
+    public float speed = 20f;
+    public SpriteRenderer flip;
+    public Collider2D playerCollider;
+    ContactFilter2D contactFilter;
+    private void Start()
     {
-        
+        target = this.transform;
+        contactFilter.SetLayerMask(LayerMask.GetMask("GroundCheck"));
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float inputX = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(speed * inputX, 0, 0);
+        if (move.x < 0)
+        {
+            flip.flipX = true;
+        }
+        if (move.x > 0)
+        {
+            flip.flipX = false;
+        }
+        move *= Time.deltaTime;
+        target.Translate(move);
+
+        if (Input.GetButtonDown("Jump") && playerCollider.IsTouching(contactFilter))
+        {
+            target.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,speed * 100));
+        }
     }
 }
