3c5e424511eda200155b40505ea74a21a3fe8406
Tangel2002 <90701100+Tangel2002@users.noreply.github.com>
2022-04-10 06:57:08 -0400

    doors enter next level

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Door : MonoBehaviour
 {
     public Collider2D hitbox;
+    public string next;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-
+            NextLevel(next);
         }
+    }
+
+    public void NextLevel(string level)
+    {
+        SceneManager.LoadScene(level);
     }
 }
